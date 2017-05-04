6b1205874d5c136df44df8f7200a7af971f0f5a6
15017082 <15017082@myrp.edu.sg>
2017-05-04 13:34:03 +0800

    From Juble

--- app/src/main/java/com/example/a15017206/p03psclassjournal/InfoActivity.java
+++ app/src/main/java/com/example/a15017206/p03psclassjournal/InfoActivity.java
@@ -10,8 +10,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 
+import static android.R.id.message;
+
 public class InfoActivity extends AppCompatActivity {
-    Button btnInfo;
+    Button btnInfo, btnEmail;
     ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class InfoActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Info for C347");
 
         btnInfo = (Button) findViewById(R.id.infoButton);
+        btnEmail = (Button) findViewById(R.id.emailButton);
         lv=(ListView)findViewById(R.id.lvInfo);
 
         LayoutInflater myinflater = getLayoutInflater();
@@ -36,8 +39,34 @@ public class InfoActivity extends AppCompatActivity {
                 // Intent to display data
                 Intent rpIntent = new Intent(Intent.ACTION_VIEW);
                 // Set the URL to be used.
-                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg/Full_Time_Courses/04_Infocomm/Diploma_in_Mobile_Software_Development_(R47).aspx"));
                 startActivity(rpIntent);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"15017082@myrp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,
+                        "Remarks so far");
+                int count = lv.getCount();
+                for(int i = 0; i < count; i++){
+                    String message = "week" + (i+1) + "DG:" + lv.getItemAtPosition(i);
+                }
+                email.putExtra(Intent.EXTRA_TEXT, message);
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
             }
         });
 
