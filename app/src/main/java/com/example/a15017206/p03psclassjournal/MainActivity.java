7fe1513f0c4a073ac2982ff7a7482a4d3e7dce66
15017206 <15017206@myrp.edu.sg>
2017-05-04 12:57:27 +0800

    Some text

--- app/src/main/java/com/example/a15017206/p03psclassjournal/MainActivity.java
+++ app/src/main/java/com/example/a15017206/p03psclassjournal/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.a15017206.p03psclassjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         lv1 = (ListView) findViewById(R.id.lv1);
 
+        //Create modules in Modules array
+        module = new ArrayList<Modules>();
+        module.add(new Modules("c300"));
+
+        //Link activity row.xml to this
+        aa = new ModuleAdapter(this, R.layout.row_activity1_listview, module);
+        lv1.setAdapter(aa);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Modules selectedModule = module.get(position);
+
+                Toast.makeText(MainActivity.this, ""+selectedModule.getModuleCode(), Toast.LENGTH_SHORT).show();
+                String sendOver = selectedModule.getModuleCode();
+
+                Intent i = new Intent();
+                i.putExtra("ModuleCode",sendOver);
+                setResult(RESULT_OK, i);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/a15017206/p03psclassjournal/ModuleAdapter.java
+++ app/src/main/java/com/example/a15017206/p03psclassjournal/ModuleAdapter.java
@@ -1,9 +1,54 @@
 package com.example.a15017206.p03psclassjournal;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by 15017206 on 04/05/2017.
  */
 
-public class ModuleAdapter {
-    
+public class ModuleAdapter extends ArrayAdapter<Modules> {
+    private ArrayList<Modules> modules;
+    private Context context;
+    private TextView tvModuleCode;
+
+    public ModuleAdapter(Context context, int resource, ArrayList<Modules> objects) {
+        super(context, resource, objects);
+        modules = objects;
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row_activity1_listview, parent, false);
+
+        // Get the TextView object - in row.xml
+        tvModuleCode = (TextView) rowView.findViewById(R.id.tvRowTemplate);
+
+        // The parameter "position" is the index of the
+        //  row ListView is requesting.
+        //  We get back the food at the same index.
+        Modules currentModule = modules.get(position);
+        // Set the TextView to show the food
+        tvModuleCode.setText(currentModule.getModuleCode());
+
+        return rowView;
+//        return super.getView(position, convertView, parent);
+    }
 }
