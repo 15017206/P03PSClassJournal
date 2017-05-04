732782e75b471cec81683ba585f3eaefaf55f571
15017359 <15017359@myrp.edu.sg>
2017-05-04 13:11:40 +0800

    ok

---
+++ app/src/main/java/com/example/a15017206/p03psclassjournal/Info.java
@@ -0,0 +1,21 @@
+package com.example.a15017206.p03psclassjournal;
+
+/**
+ * Created by 15017359 on 4/5/2017.
+ */
+
+public class Info {
+    private String name;
+    private boolean book;
+    public Info(String name, boolean book) {
+        this.name = name;
+        this.book = book;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isBook() {
+        return book;
+    }
+
+}
