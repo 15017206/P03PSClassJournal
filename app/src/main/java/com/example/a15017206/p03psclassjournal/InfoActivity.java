efeb83725c1e28ac7c9e53617c2da91386cec853
15017082 <15017082@myrp.edu.sg>
2017-05-04 12:27:11 +0800

    From Juble

--- app/src/main/java/com/example/a15017206/p03psclassjournal/InfoActivity.java
+++ app/src/main/java/com/example/a15017206/p03psclassjournal/InfoActivity.java
@@ -1,13 +1,32 @@
 package com.example.a15017206.p03psclassjournal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
+    Button btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btnInfo = (Button) findViewById(R.id.infoButton);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
     }
+
 }
